6bbaa9fde9349371d213602e741536d1d776955a
MiguelRomero2020 <70560507+MiguelRomero2020@users.noreply.github.com>
2025-07-19 16:24:25 -0500

    Add files via upload

---
+++ js/scriptdos.js
@@ -0,0 +1,15 @@
+function scrollUp() {
+  window.scrollBy({
+    top: -500,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+function scrollDown() {
+  window.scrollBy({
+    top: 500,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
